982d7c580c1af46c06ccb03f64da6836a6db477e
Lucas Ibañez <lucas.ibanez@gfe.frre.utn.edu.ar>
2021-09-30 21:44:08 -0300

    grafico x jurisdiccion

--- static/js/graficos/vacunas_x_jurisdiccion.js
+++ static/js/graficos/vacunas_x_jurisdiccion.js
@@ -1,8 +1,7 @@
 am4core.ready(function() {
 
 // Themes begin
-am4core.useTheme(am4themes_dark);
-am4core.useTheme(am4themes_animated);
+am4core.useTheme(am4themes_kelly);
 // Themes end
 
 /**
@@ -14,34 +13,10 @@ chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
 
 chart.paddingRight = 40;
 
-chart.data = [{
-    "name": "Monica",
-    "steps": 45688,
-    "href": "https://www.amcharts.com/wp-content/uploads/2019/04/monica.jpg"
-}, {
-    "name": "Joey",
-    "steps": 35781,
-    "href": "https://www.amcharts.com/wp-content/uploads/2019/04/joey.jpg"
-}, {
-    "name": "Ross",
-    "steps": 25464,
-    "href": "https://www.amcharts.com/wp-content/uploads/2019/04/ross.jpg"
-}, {
-    "name": "Phoebe",
-    "steps": 18788,
-    "href": "https://www.amcharts.com/wp-content/uploads/2019/04/phoebe.jpg"
-}, {
-    "name": "Rachel",
-    "steps": 15465,
-    "href": "https://www.amcharts.com/wp-content/uploads/2019/04/rachel.jpg"
-}, {
-    "name": "Chandler",
-    "steps": 11561,
-    "href": "https://www.amcharts.com/wp-content/uploads/2019/04/chandler.jpg"
-}];
+chart.data = JSON.parse(dosis1)
 
 var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-categoryAxis.dataFields.category = "name";
+categoryAxis.dataFields.category = "jurisdiccion";
 categoryAxis.renderer.grid.template.strokeOpacity = 0;
 categoryAxis.renderer.minGridDistance = 10;
 categoryAxis.renderer.labels.template.dx = -40;
@@ -58,9 +33,9 @@ valueAxis.renderer.baseGrid.strokeOpacity = 0;
 valueAxis.renderer.labels.template.dy = 20;
 
 var series = chart.series.push(new am4charts.ColumnSeries);
-series.dataFields.valueX = "steps";
-series.dataFields.categoryY = "name";
-series.tooltipText = "{valueX.value}";
+series.dataFields.valueX = "porcentaje_vacunados";
+series.dataFields.categoryY = "jurisdiccion";
+series.tooltipText = "{valueX.value} %";
 series.tooltip.pointerOrientation = "vertical";
 series.tooltip.dy = - 30;
 series.columnsContainer.zIndex = 100;
@@ -110,6 +85,107 @@ image.adapter.add("mask", function (mask, target) {
 
 var previousBullet;
 chart.cursor.events.on("cursorpositionchanged", function (event) {
+    var dataItem = series.tooltipDataItem;
+
+    if (dataItem.column) {
+        var bullet = dataItem.column.children.getIndex(1);
+
+        if (previousBullet && previousBullet != bullet) {
+            previousBullet.isHover = false;
+        }
+
+        if (previousBullet != bullet) {
+
+            var hs = bullet.states.getKey("hover");
+            hs.properties.dx = dataItem.column.pixelWidth;
+            bullet.isHover = true;
+
+            previousBullet = bullet;
+        }
+    }
+})
+
+/* ================================
+            2da Dosis
+   ================================*/
+
+var chart2 = am4core.create("chartdiv2", am4charts.XYChart);
+chart2.hiddenState.properties.opacity = 0; // this creates initial fade-in
+
+chart2.paddingRight = 40;
+
+chart2.data = JSON.parse(dosis2)
+
+var categoryAxis = chart2.yAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "jurisdiccion";
+categoryAxis.renderer.grid.template.strokeOpacity = 0;
+categoryAxis.renderer.minGridDistance = 10;
+categoryAxis.renderer.labels.template.dx = -40;
+categoryAxis.renderer.minWidth = 120;
+categoryAxis.renderer.tooltip.dx = -40;
+
+var valueAxis = chart2.xAxes.push(new am4charts.ValueAxis());
+valueAxis.renderer.inside = true;
+valueAxis.renderer.labels.template.fillOpacity = 0.3;
+valueAxis.renderer.grid.template.strokeOpacity = 0;
+valueAxis.min = 0;
+valueAxis.cursorTooltipEnabled = false;
+valueAxis.renderer.baseGrid.strokeOpacity = 0;
+valueAxis.renderer.labels.template.dy = 20;
+
+var series = chart2.series.push(new am4charts.ColumnSeries);
+series.dataFields.valueX = "porcentaje_vacunados";
+series.dataFields.categoryY = "jurisdiccion";
+series.tooltipText = "{valueX.value} %";
+series.tooltip.pointerOrientation = "vertical";
+series.tooltip.dy = - 30;
+series.columnsContainer.zIndex = 100;
+
+var columnTemplate = series.columns.template;
+columnTemplate.height = am4core.percent(50);
+columnTemplate.maxHeight = 50;
+columnTemplate.column.cornerRadius(60, 10, 60, 10);
+columnTemplate.strokeOpacity = 0;
+
+series.heatRules.push({ target: columnTemplate, property: "fill", dataField: "valueX", min: am4core.color("#e5dc36"), max: am4core.color("#5faa46") });
+series.mainContainer.mask = undefined;
+
+var cursor = new am4charts.XYCursor();
+chart2.cursor = cursor;
+cursor.lineX.disabled = true;
+cursor.lineY.disabled = true;
+cursor.behavior = "none";
+
+var bullet = columnTemplate.createChild(am4charts.CircleBullet);
+bullet.circle.radius = 30;
+bullet.valign = "middle";
+bullet.align = "left";
+bullet.isMeasured = true;
+bullet.interactionsEnabled = false;
+bullet.horizontalCenter = "right";
+bullet.interactionsEnabled = false;
+
+var hoverState = bullet.states.create("hover");
+var outlineCircle = bullet.createChild(am4core.Circle);
+outlineCircle.adapter.add("radius", function (radius, target) {
+    var circleBullet = target.parent;
+    return circleBullet.circle.pixelRadius + 10;
+})
+
+var image = bullet.createChild(am4core.Image);
+image.width = 60;
+image.height = 60;
+image.horizontalCenter = "middle";
+image.verticalCenter = "middle";
+image.propertyFields.href = "href";
+
+image.adapter.add("mask", function (mask, target) {
+    var circleBullet = target.parent;
+    return circleBullet.circle;
+})
+
+var previousBullet;
+chart2.cursor.events.on("cursorpositionchanged", function (event) {
     var dataItem = series.tooltipDataItem;
 
     if (dataItem.column) {
